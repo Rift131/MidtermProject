80d79e9359c36f8864f1bf3097749827369350c2
Kenneth Pederson <kenneth.pederson@yahoo.com>
2022-08-18 12:59:00 -0500

    Created Boot MVC Project, linked JPA dependency

---
+++ CakeBids/src/main/java/com/skilldistillery/cakebids/CakeBidsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cakebids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeBidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeBidsApplication.class, args);
+	}
+
+}
---
+++ CakeBids/src/test/java/com/skilldistillery/cakebids/CakeBidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cakebids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeBidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
